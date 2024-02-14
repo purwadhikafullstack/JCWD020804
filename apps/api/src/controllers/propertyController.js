17d457259e983cbf458b8c7588969dbe02c8e2a1
Syamsuddin Nur <syamsuddinnur.sn@gmail.com>
2024-02-14 10:18:08 +0700

    fix(bug-fixing): path appjs

--- apps/api/src/controllers/propertyController.js
+++ apps/api/src/controllers/propertyController.js
@@ -160,7 +160,7 @@ export const addProperty = async (req, res) => {
     if (req?.file) {
       const fileName = req?.file?.filename;
       const URL = process.env.VITE_IMAGE_URL;
-      file = `${URL}/${fileName}`;
+      file = `${URL}${fileName}`;
     }
     const property = await Property.create({
       name,
@@ -192,7 +192,7 @@ export const editProperty = async (req, res) => {
     if (req?.file) {
       const fileName = req?.file?.filename;
       const URL = process.env.VITE_IMAGE_URL;
-      file = `${URL}/${fileName}`;
+      file = `${URL}${fileName}`;
     }
     await Property.update(
       {
